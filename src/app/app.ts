ee5425996e5ac9cf8b22c5b5c2f7169fb174e7aa
Agustin <agustindelossantos023@gmail.com>
2025-10-20 09:53:10 +0200

    ADD footer-c

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterC } from "./shared/components/footer-c/footer-c";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterC],
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('country-app');
+
+
 }
--- src/app/shared/pages/home-p/home-p.ts
+++ src/app/shared/pages/home-p/home-p.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-p',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-p.html',
   styleUrl: './home-p.css'
 })
