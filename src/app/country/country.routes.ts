a9705d6ee7469968bb643132b1c2cb15b9e154f2
Agustin <agustindelossantos023@gmail.com>
2025-10-20 09:12:00 +0200

    ADD country-layout-c

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,18 +1,29 @@
 import { Routes } from '@angular/router';
+import { CountryLayoutC } from './layouts/country-layout-c/country-layout-c';
 import { ByCapitalP } from './pages/by-capital-p/by-capital-p';
 
 const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalP
+    component: CountryLayoutC,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalP
+      },
+      // {
+
+      // },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   },
   // {
   //   path: 'country',
   // },
-  // {
-  //   path: '**',
-  //   redirectTo: ''
-  // }
+
 ];
 
 
---
+++ src/app/country/layouts/country-layout-c/country-layout-c.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout-c',
+  imports: [ RouterOutlet ],
+  templateUrl: './country-layout-c.html',
+  styleUrl: './country-layout-c.css'
+})
+export class CountryLayoutC {
+
+}
